b2556683290643646354520d2102e0d7a528d521
unknown <kylespinaio@hotmail.com>
2020-07-09 22:49:03 -0400

    added working for loop that appends and displays the array data to the table

--- app.js
+++ app.js
@@ -3,16 +3,32 @@
 
 $(document).ready(function(){
     $.ajax({
-        url:"www.devcodecampmusiclibrary.com/api/music",
+        crossDomain:true,
+        contentType:"application/json",
+        url:"http://www.devcodecampmusiclibrary.com/api/music",
         dataType:"json",
         type: "get",
-        success: function(data, meta){
-            for(let i = 0; i<data.result.length; i++)
-            {
-                $("music").append(`<div>${data.result[i]["title"]}</div`);
-            }
+        
+        success: function(data, meta, third){
+            // let result = JSON.stringify(data)
+            for (let i = 0; i < data.length; i++){
+                    $("#musicTable").find('tbody').append(`<tr><td>Title</td><td>Album</td><td>Artist</td></tr>`);
+                }
+
+
+            
+            // console.log(result)
+
+
+
+
+
+           //for(let i = 0; i<data.result.length; i++)
+            // {
+            //     //$("music").append(`<div>${data.result[i]["title"]}</div`);
+            // }
         },
-        error: function(meta, errorThrown){
+        error: function(meta, errorThrown, third){
             console.log(errorThrown);
         }
 
