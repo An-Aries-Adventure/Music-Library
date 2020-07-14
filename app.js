ea105b15bad5197e16a4d5ee165828e318543691
unknown <kylespinaio@hotmail.com>
2020-07-13 21:06:53 -0400

    added postman get by id. saved as JSON file

--- app.js
+++ app.js
@@ -14,21 +14,8 @@ $(document).ready(function(){
             for (let i = 0; i < data.length; i++){
                     $("#musicTable").find('tbody').append(`<tr><td>${data[i]["title"]}</td><td>${data[i]["album"]}</td><td>${data[i]["artist"]}</td><td>${data[i]["genre"]}</td><td>${data[i]["releaseDate"]}</td></tr>`)
                     
-                    // <tr><td>data[i]</td><td>data.Album[i]</td><td>data.Artist[i]</td></tr>);["Title"]
+
                 }
-
-
-            
-            // console.log(result)
-
-
-
-
-
-           //for(let i = 0; i<data.result.length; i++)
-            // {
-            //     //$("music").append(`<div>${data.result[i]["title"]}</div`);
-            // }
         },
         error: function(meta, errorThrown, third){
             console.log(errorThrown);
@@ -39,3 +26,5 @@ $(document).ready(function(){
 
     })
 })
+
+// <tr display="none">${id}</tr> 
